742fcc202cf75bd7f76274ee3625521dcf4d6390
bandicoutts <d.coutts@gmail.com>
2019-02-13 22:34:36 +1300

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -1,2 +1,14 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+#item-container contains all the relevant information
+
+myURL = 'https://www.newegg.com/Desktop-Memory/SubCategory/ID-147?Tid=7611'
+
 def main():
-    pass
+    uClient = uReq(myURL)
+    results = uClient.read()
+    uClient.close()
+    pageSoup = soup(results, "html.parser")
+    containers = pageSoup.findAll("div", {"class":"item-container"})
+    
