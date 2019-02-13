600a69804301116dc57dacc70089ddfe2bead6a1
bandicoutts <d.coutts@gmail.com>
2019-02-13 23:03:26 +1300

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 #item-container contains all the relevant information
+# will take name of product, price
 
 myURL = 'https://www.newegg.com/Desktop-Memory/SubCategory/ID-147?Tid=7611'
 
@@ -11,4 +12,15 @@ def main():
     uClient.close()
     pageSoup = soup(results, "html.parser")
     containers = pageSoup.findAll("div", {"class":"item-container"})
-    
+
+    for i in containers:
+        titleContainer = i.findAll("a", {"class": "item-title"})
+        productName = titleContainer[0].text
+        titlePrice = i.findAll("li", {"class": "price-current"})
+        productPrice = titlePrice[0].text.strip()
+
+        print(productName, productPrice)
+
+
+
+main()
